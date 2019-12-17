b7d9bfbdc2e0922971d9b39f9e4f3640e02b50d5
Kamila2323 <kamila.zwiernik@gmail.com>
2019-12-17 18:32:50 +0100

    Podaj metraż nieruchomości

--- src/KalkulatorKredytowyTest.java
+++ src/KalkulatorKredytowyTest.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class KalkulatorKredytowyTest {
@@ -6,5 +7,8 @@ public class KalkulatorKredytowyTest {
 
         Scanner input = new Scanner(System.in);
 
+        //Podaj metraż nieruchomości
+        System.out.println("Podaj metraż nieruchomości w m2:");
+        BigDecimal metrazNieruchomosci = input.nextBigDecimal();
     }
 }
